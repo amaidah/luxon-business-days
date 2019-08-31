e6e61ce03c15a608b3faa3bf8b7be40d8cbfbaf6
Andrew Maidah <a.maidah@gmail.com>
2019-08-30 19:49:11 -0700

    Remove getter / setter approach

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ if (typeof require === 'function') {
 
 const DEFAULT_BUSINESS_DAYS = [1, 2, 3, 4, 5];
 
-// Object.defineProperty(DateTime, 'businessDays', {
-//   get: function() {
-//     return this.businessDays;
-//   },
-//   set: function(newBusinessDays) {
-//     this.businessDays = newBusinessDays;
-//   },
-// });
-
-// DateTime.prototype.defineBusinessDays = function(businessDays) {
-//   this.businessDays = businessDays;
-// };
-
-// DateTime.prototype.getBusinessDays = function() {
-//   console.log(this.businessDays);
-//   return this.businessDays;
-// };
-
 DateTime.prototype.setupBusiness = function(config) {
   const { businessDays, holidays } = config;
 
